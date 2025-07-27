4b17d2685db9265e85512cd75ff420533457583e
Dhanush KL <dhanuchandu1232@gmail.com>
2025-07-27 09:49:50 +0530

    new 3

--- src/components/CTA.tsx
+++ src/components/CTA.tsx
@@ -1,21 +1,18 @@
 import styles from './CTA.module.css';
-import Dither from './Dither';
+import Silk from './Silk';
 
 export default function CTA() {
   return (
     <section className={styles.ctaContainer}>
       {/* Rectangular background (60% height) */}
-      <div className={styles.ditherBackground}>
-        <Dither
-          waveColor={[0.5, 0.5, 0.5]}
-          disableAnimation={false}
-          enableMouseInteraction={true}
-          mouseRadius={0.3}
-          colorNum={4}
-          waveAmplitude={0.3}
-          waveFrequency={3}
-          waveSpeed={0.05}
-        />
+      <div style={{ width: '100%', height: '600px', position: 'relative' }}>
+       <Silk
+  speed={5}
+  scale={1}
+  color="#7B7481"
+  noiseIntensity={1.5}
+  rotation={0}
+/>
       </div>
 
       {/* Content directly on glass */}
